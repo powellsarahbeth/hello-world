4fbabbcd5459f338a2664181f73e803842718ff0
powellsarahbeth <powellsarahbeth@gmail.com>
2022-09-28 22:05:55 -0600

    upload new python files

---
+++ draw.py
@@ -0,0 +1,13 @@
+import turtle
+painter = turtle.Turtle()
+<<<<<<< HEAD
+
+for _ in range(18):
+	painter.forward(100)
+	painter.left(100)
+
+=======
+painter.forward(100)
+painter.left(90)
+>>>>>>> parent of 0f0c59b (repeat 4 times with a for loop)
+turtle.done()
